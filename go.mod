ab6d123ae701ee11f6d35387cd1cf5b84ea9791c
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-17 02:11:29 -0400

    Adding repository implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wilo087/feds.git
 
 go 1.20
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ models/feed.go
@@ -0,0 +1,9 @@
+package models
+
+type Feed struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
