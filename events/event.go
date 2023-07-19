57672412c4f1fd574b92ffe7f6f7abbdac386253
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-19 17:40:10 -0400

    Defining messages for Events

---
+++ events/event.go
@@ -0,0 +1,32 @@
+package events
+
+import (
+	"context"
+
+	"github.com/wilo087/feds.git/models"
+)
+
+type EventStore interface {
+	Close()
+	PublishCreadtedFeed(ctx context.Context, feed *models.Feed) error
+	SubscribeCreatedFeed(ctx context.Context) (<-chan *CreatedFeedMessage, error)
+	OnCreatedFeed(handler func(CreatedFeedMessage)) error
+}
+
+var eventStore EventStore
+
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreadtedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreadtedFeed(ctx, feed)
+}
+
+func SubscribeCreatedFeed(ctx context.Context) (<-chan *CreatedFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+
+func OnCreatedFeed(ctx context.Context, handler func(CreatedFeedMessage)) error {
+	return eventStore.OnCreatedFeed(handler)
+}
---
+++ events/messages.go
@@ -0,0 +1,17 @@
+package events
+
+type Message interface {
+	Type() string
+}
+
+type CreatedFeedMessage struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+func (m *CreatedFeedMessage) Type() string {
+	return "feed.created"
+}
