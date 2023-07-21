dee9ddbf4ddbbfc199bd73233b7f33b98a4cd5a5
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-20 20:47:44 -0400

    Adding channels for subcriber method

--- events/nats.go
+++ events/nats.go
@@ -61,3 +61,43 @@ func (n *NatsEventStore) PublishCreadtedFeed(ctx context.Context, feed *models.F
 
 	return n.conn.Publish(msg.Type(), data)
 }
+
+func (n *NatsEventStore) decodeMessage(data []byte, m interface{}) error {
+	buffer := bytes.NewBuffer(data)
+	return gob.NewDecoder(buffer).Decode(m)
+}
+
+func (n NatsEventStore) OnCreatedFeed(f func(CreatedFeedMessage)) (err error) {
+	msg := CreatedFeedMessage{}
+	n.feedCreatedSub, err = n.conn.Subscribe(msg.Type(), func(m *nats.Msg) {
+		n.decodeMessage(m.Data, &msg)
+		f(msg)
+	})
+
+	return
+}
+
+func (n *NatsEventStore) SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	msg := CreatedFeedMessage{}
+	n.feedCreatedCh = make(chan CreatedFeedMessage, 64)
+	ch := make(chan *nats.Msg, 64)
+
+	var err error
+	n.feedCreatedSub, err = n.conn.ChanSubscribe(msg.Type(), ch)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			select {
+			case m := <-ch:
+				n.decodeMessage(m.Data, &msg)
+				n.feedCreatedCh <- msg
+			}
+		}
+	}()
+
+	return (<-chan CreatedFeedMessage)(n.feedCreatedCh), nil
+	// return n.feedCreatedCh, nil
+}
