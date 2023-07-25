60f2c0ee6e5ce9232cfd3026d1e0e2d151e3ed36
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-25 01:31:06 -0400

    Adding new abstract implementation

--- search/repository.go
+++ search/repository.go
@@ -9,5 +9,23 @@ import (
 type SearchRepository interface {
 	Close()
 	InsertFeed(ctx context.Context, feed *models.Feed) error
-	SearchFeed(ctx context.Context, query string) ([]*models.Feed, error)
+	SearchFeed(ctx context.Context, query string) ([]models.Feed, error)
+}
+
+var repo SearchRepository
+
+func SetRepository(r SearchRepository) {
+	repo = r
+}
+
+func Close() {
+	repo.Close()
+}
+
+func InsertFeed(ctx context.Context, feed *models.Feed) error {
+	return repo.InsertFeed(ctx, feed)
+}
+
+func SearchFeed(ctx context.Context, query string) ([]models.Feed, error) {
+	return repo.SearchFeed(ctx, query)
 }
